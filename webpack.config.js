11c8b2b5f38477524bf9c611b05df405ffef8886
Priyansh Agrawal <agrawal.priyansh@yahoo.in>
2023-07-15 00:46:50 +0100

    Bring down build size...
    ...by separating the dev build and the prod build. prod build is now
    built with production environment bringing its size down from ~1.6MB
    to ~250KB.

--- webpack.config.js
+++ webpack.config.js
@@ -3,49 +3,63 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'docs'),
-        clean: true,
-        publicPath: "",
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, "views", "index.html"),
-            favicon: path.join(__dirname, "views", "favicon.ico"),
-        }),
-        new MiniCssExtractPlugin(),
-        new CopyPlugin({
-            patterns: [{
-                from: path.join(__dirname, 'public'),
-                to: path.join(__dirname, 'docs'),
-            }]
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ["babel-loader"],
-            },
-            {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
-            },
-            {
-                test: /\.(png|jpg|gif|ico|svg)$/i,
-                type: 'asset/resource',
-            },
-        ],
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'docs'),
+const BUILD_DIR = {
+    production: 'docs',  // needed for github-pages
+    development: 'build',
+}
+
+module.exports = (env, argv) => {
+    const environment = argv.mode;
+
+    if (!Object.keys(BUILD_DIR).includes(environment))
+        throw `Unsupported environment "${environment}"`
+
+    const buildDir = BUILD_DIR[environment];
+
+    return {
+        entry: './src/index.js',
+        output: {
+            filename: 'main.js',
+            path: path.resolve(__dirname, buildDir),
+            clean: true,
+            publicPath: "",
         },
-        port: 3000,
-        historyApiFallback: true,
-    },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.join(__dirname, "views", "index.html"),
+                favicon: path.join(__dirname, "views", "favicon.ico"),
+            }),
+            new MiniCssExtractPlugin(),
+            new CopyPlugin({
+                patterns: [{
+                    from: path.join(__dirname, 'public'),
+                    to: path.join(__dirname, buildDir),
+                }]
+            }),
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: ["babel-loader"],
+                },
+                {
+                    test: /\.css$/i,
+                    use: [MiniCssExtractPlugin.loader, "css-loader"],
+                },
+                {
+                    test: /\.(png|jpg|gif|ico|svg)$/i,
+                    type: 'asset/resource',
+                },
+            ],
+        },
+        devServer: {
+            static: {
+                directory: path.join(__dirname, buildDir),
+            },
+            port: 3000,
+            historyApiFallback: true,
+        },
+    };
 };
