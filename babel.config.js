fb985b04f33b9f35b3154625fbd429637ccc52c3
Priyansh Agrawal <agrawal.priyansh@yahoo.in>
2023-07-14 01:26:22 +0100

    Set correct base url in index.html

--- babel.config.js
+++ babel.config.js
@@ -4,9 +4,9 @@ module.exports = function (api) {
     const presets = [];
 
     // Don't do things like minification in dev for easy debugging.
-    if (process.env.NODE_ENV !== 'development') {
-        presets.push("@babel/preset-env");
-    }
+    // if (process.env.NODE_ENV !== 'development') {
+    //     presets.push("@babel/preset-env");
+    // }
 
     presets.push(["@babel/preset-react", {
         "runtime": "automatic"
--- webpack.config.js
+++ webpack.config.js
@@ -3,49 +3,52 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'docs'),
-        clean: true,
-        publicPath: "/",
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, "views", "index.html"),
-            favicon: path.join(__dirname, "views", "favicon.ico"),
-        }),
-        new MiniCssExtractPlugin(),
-        new CopyPlugin({
-            patterns: [{
-                from: path.join(__dirname, 'public'),
-                to: path.join(__dirname, 'docs'),
-            }]
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ["babel-loader"],
-            },
-            {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
-            },
-            {
-                test: /\.(png|jpg|gif|ico|svg)$/i,
-                type: 'asset/resource',
-            },
-        ],
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, "docs"),
+module.exports = (env, argv) => {
+    return {
+        entry: './src/index.js',
+        output: {
+            filename: 'main.js',
+            path: path.resolve(__dirname, 'docs'),
+            clean: true,
+            publicPath: "/",
         },
-        port: 3000,
-        historyApiFallback: true,
-    },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.join(__dirname, "views", "index.html"),
+                favicon: path.join(__dirname, "views", "favicon.ico"),
+                base: argv.mode === 'production' ? 'https://agrawpri.github.io/quote-generator/' : 'http://localhost:3000/',
+            }),
+            new MiniCssExtractPlugin(),
+            new CopyPlugin({
+                patterns: [{
+                    from: path.join(__dirname, 'public'),
+                    to: path.join(__dirname, 'docs'),
+                }]
+            }),
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: ["babel-loader"],
+                },
+                {
+                    test: /\.css$/i,
+                    use: [MiniCssExtractPlugin.loader, "css-loader"],
+                },
+                {
+                    test: /\.(png|jpg|gif|ico|svg)$/i,
+                    type: 'asset/resource',
+                },
+            ],
+        },
+        devServer: {
+            static: {
+                directory: path.join(__dirname, "docs"),
+            },
+            port: 3000,
+            historyApiFallback: true,
+        },
+    };
 };
